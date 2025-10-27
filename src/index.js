65c0c82db8bc63865b037484a02749f1564ac8db
Hyacinth-Chidi <mmadubugwujoseph@gmail.com>
2025-10-27 16:51:14 +0100

    Update CORS configuration to include vercel deployment URL

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://unimartz.vercel.app',
   credentials: true, // Allow cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
